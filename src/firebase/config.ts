675fde09e717371529b841167b02c9d4a3c40d53
SANVI RAJAN UDHAN <f20230817@hyderabad.bits-pilani.ac.in>
2025-12-16 06:37:24 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7314681269-f319b",
+  "appId": "1:679414676910:web:9d52f392ee3b3fc0b55c3f",
+  "apiKey": "AIzaSyADDuW-ha33zyHJO3EFIKcalNAB217G8To",
+  "authDomain": "studio-7314681269-f319b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "679414676910"
+};
